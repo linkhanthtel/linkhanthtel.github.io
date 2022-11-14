7b4c695972e5b0b5bf2e70361fdfbf64d7db1177
Lin Khant Htel <linkhanthtel@icloud.com>
2022-11-14 23:23:27 +0630

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,4 +1,4 @@
-const nom = document.getElementById('name').value;
+const nom = document.getElementById('name').values;
 
 document.getElementById('submit').onclick = function() {
     document.getElementById('contactinfo').innerHTML = `Dear ${nom} ! <br><br> Thank you for your message! I will get back to you soon! <br><br> <a href='index.html'>Back to home</a>`;
