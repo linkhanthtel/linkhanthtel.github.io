2f1a01d0fae68c11dc2cb6ecb4fd1f01b349563d
Lin Khant Htel <linkhanthtel@icloud.com>
2023-04-14 13:35:45 +0630

    Delete contact.js

--- contact.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('submit').onclick = function() {
-    const nom = document.getElementById('name').value;
-    document.getElementById('contactinfo').innerHTML = `Dear ${nom} ! <br><br> Thank you for your message! I will get back to you soon! <br><br> <a href='index.html'>Back to home</a>`;
-    document.getElementById('contactinfo').style.textAlign = 'center';
-    document.getElementById('contactinfo').style.fontFamily = "'Times New Roman', Times, serif";
-}
