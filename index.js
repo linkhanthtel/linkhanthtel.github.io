9c83beceb2de3d6bd55eeb10c53df21e4d5289c9
Lin Khant Htel <linkhanthtel@icloud.com>
2023-08-02 19:45:30 +0630

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ $('.welcomepage h1').click(function() {
     alert("Nice to meet you! Thanks for visiting my site")
 });
 
+$('#lang').click(function() {
+    alert("Burmese version is not available yet")
+})
+
 document.getElementById('submit').onclick = function() {
     const nom = document.getElementById('name').value;
     document.getElementById('contactinfo').innerHTML = `Dear ${nom} ! <br><br> Thank you for your message! I will get back to you soon!`;
     document.getElementById('contactinfo').style.textAlign = 'center';
     document.getElementById('contactinfo').style.fontFamily = "'Times New Roman', Times, serif";
 }
+
+$("#dropdownmenu").click(function() {
+    $("#dropdown").fadeToggle();
+})
