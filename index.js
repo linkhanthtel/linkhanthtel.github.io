968adf8fefbc4e39c9291c761e1a37e53797b012
Lin Khant Htel <linkhanthtel@icloud.com>
2023-12-28 16:31:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ $('.welcomepage h1').click(function() {
     alert("Nice to meet you! Thanks for visiting my site")
 });
 
+$("#dropdownmenu").click(function() {
+    $("#dropdown").fadeToggle();
+})
+
 $('#lang').click(function() {
     alert("Burmese version is not available yet")
 })
 
-document.getElementById('submit').onclick = function() {
-    const nom = document.getElementById('name').value;
-    document.getElementById('contactinfo').innerHTML = `Dear ${nom} ! <br><br> Thank you for your message! I will get back to you soon!`;
-    document.getElementById('contactinfo').style.textAlign = 'center';
-    document.getElementById('contactinfo').style.fontFamily = "'Times New Roman', Times, serif";
-}
-
-$("#dropdownmenu").click(function() {
-    $("#dropdown").fadeToggle();
-})
+// document.getElementById('submit').onclick = function() {
+//     const nom = document.getElementById('name').value;
+//     document.getElementById('contactinfo').innerHTML = `Dear ${nom} ! <br><br> Thank you for your message! I will get back to you soon!`;
+//     document.getElementById('contactinfo').style.textAlign = 'center';
+//     document.getElementById('contactinfo').style.color = 'rgb(2,0,40)';
+//     document.getElementById('contactinfo').style.fontFamily = "'Times New Roman', Times, serif";
+// }
